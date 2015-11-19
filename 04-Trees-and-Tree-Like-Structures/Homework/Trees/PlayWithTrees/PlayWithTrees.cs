cb04dae374b2dc6b9ed1ebb786983cbd3e2aaa80
tockata <anatoly.angelov@gmail.com>
2015-11-19 23:57:51 +0200

    Added Problem 1 partly solution.

---
+++ 04-Trees-and-Tree-Like-Structures/Homework/Trees/PlayWithTrees/PlayWithTrees.cs
@@ -0,0 +1,67 @@
+﻿namespace PlayWithTrees
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PlayWithTrees
+    {
+        private static Dictionary<int, Tree<int>> nodeByValue = new Dictionary<int, Tree<int>>();
+
+        public static void Main(string[] args)
+        {
+            int nodesCount = int.Parse(Console.ReadLine());
+            for (int i = 1; i < nodesCount; i++)
+            {
+                string[] edge = Console.ReadLine().Split(' ');
+                int parentValue = int.Parse(edge[0]);
+                Tree<int> parentNode = GetTreeNodeByValue(parentValue);
+                int childValue = int.Parse(edge[1]);
+                Tree<int> childNode = GetTreeNodeByValue(childValue);
+                parentNode.Children.Add(childNode);
+                childNode.Parent = parentNode;
+            }
+
+            int pathSum = int.Parse(Console.ReadLine());
+            int subtreeSum = int.Parse(Console.ReadLine());
+
+            var rootNode = FindRootNode();
+            var leafNodes = FindLeafNodes();
+            var middleNodes = FindMiddleNodes();
+        }
+
+        private static IEnumerable<Tree<int>> FindLeafNodes()
+        {
+            var leafNodes = nodeByValue.Values
+                .Where(n => !n.Children.Any() && n.Parent != null)
+                .ToList();
+
+            return leafNodes;
+        }
+
+        private static IEnumerable<Tree<int>> FindMiddleNodes()
+        {
+            var middleNodes = nodeByValue.Values
+                .Where(n => n.Children.Any() && n.Parent != null)
+                .ToList();
+
+            return middleNodes;
+        }
+
+        private static Tree<int> FindRootNode()
+        {
+            var rootNode = nodeByValue.Values.FirstOrDefault(n => n.Parent == null);
+            return rootNode;
+        }
+
+        private static Tree<int> GetTreeNodeByValue(int value)
+        {
+            if (!nodeByValue.ContainsKey(value))
+            {
+                nodeByValue[value] = new Tree<int>(value);
+            }
+
+            return nodeByValue[value];
+        }
+    }
+}
---
+++ 04-Trees-and-Tree-Like-Structures/Homework/Trees/PlayWithTrees/Tree.cs
@@ -0,0 +1,24 @@
+﻿namespace PlayWithTrees
+{
+    using System.Collections.Generic;
+
+    public class Tree<T>
+    {
+        public Tree(T value, params Tree<T>[] childrens)
+        {
+            this.Value = value;
+            this.Children = new List<Tree<T>>();
+            foreach (var child in childrens)
+            {
+                child.Parent = this;
+                this.Children.Add(child);
+            }
+        }
+
+        public IList<Tree<T>> Children { get; set; }
+
+        public Tree<T> Parent { get; set; }
+
+        public T Value { get; set; }
+    }
+}
