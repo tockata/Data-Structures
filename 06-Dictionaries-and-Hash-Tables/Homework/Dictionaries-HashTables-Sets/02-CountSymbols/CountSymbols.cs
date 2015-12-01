8b3b819bf0b7c476daa2b49f340fc321df2b95e9
tockata <anatoly.angelov@gmail.com>
2015-12-01 19:36:58 +0200

    Added some Dictionary homework solutions.

---
+++ 06-Dictionaries-and-Hash-Tables/Homework/Dictionaries-HashTables-Sets/02-CountSymbols/CountSymbols.cs
@@ -0,0 +1,29 @@
+﻿namespace _02_CountSymbols
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountSymbols
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Dictionary<char, int> charactersCount = new Dictionary<char, int>();
+            foreach (var character in input)
+            {
+                if (!charactersCount.ContainsKey(character))
+                {
+                    charactersCount[character] = 0;
+                }
+
+                charactersCount[character]++;
+            }
+
+            charactersCount
+                .OrderBy(ch => ch.Key)
+                .ToList()
+                .ForEach(character => Console.WriteLine($"{character.Key}: {character.Value} time/s"));
+        }
+    }
+}
---
+++ 06-Dictionaries-and-Hash-Tables/Homework/Dictionaries-HashTables-Sets/Dictionary/KeyValue.cs
@@ -0,0 +1,37 @@
+﻿namespace Dictionary
+{
+    public class KeyValue<TKey, TValue>
+    {
+        public KeyValue(TKey key, TValue value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public TKey Key { get; set; }
+
+        public TValue Value { get; set; }
+
+        public override bool Equals(object other)
+        {
+            var otherElement = (KeyValue<TKey, TValue>)other;
+            var equals = object.Equals(this.Key, otherElement.Key) && object.Equals(this.Value, otherElement.Value);
+            return equals;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.CombineHashCodes(this.Key.GetHashCode(), this.Value.GetHashCode());
+        }
+        
+        public override string ToString()
+        {
+            return $"[{this.Key} -> {this.Value}]";
+        }
+
+        private int CombineHashCodes(int h1, int h2)
+        {
+            return ((h1 << 5) + h1) ^ h2;
+        }
+    }
+}
